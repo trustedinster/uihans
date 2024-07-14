32b0622549a3838734a8317e362afcd14386c328
Supercmd <trustedinster@outlook.com>
2024-07-14 23:45:48 +0800

    修复了大量BUG，使用了百度翻译

--- bash.py
+++ bash.py
@@ -1,115 +1,77 @@
-from time import sleep
-from warnings import catch_warnings, simplefilter
-from xml.sax.saxutils import unescape
-from random import random
 from os import listdir
 from os.path import isfile, splitext
 from pickle import dump, load
+from random import randint
+from hashlib import md5
+from logging import basicConfig, warning, error, debug, DEBUG
+from datetime import datetime
 
-from requests import get
-from requests.exceptions import JSONDecodeError
 from easygui import fileopenbox, filesavebox, diropenbox
-from requests import get
+from requests import post
 import xml.etree.ElementTree as ET
 from tqdm import tqdm
 
+basicConfig(level=DEBUG, format='%(asctime)s [%(levelname)s] %(module)s %(message)s',
+            datefmt='%Y-%m-%d %A %H:%M:%S', filename="{}.log".format(datetime.now().strftime('%Y%m%d')),
+            filemode='a')
 
 USR_DICT = {"test": "0test0"}
+ERR_TRIES = 3
 
 if "settings.pkl" in listdir():
     with open("settings.pkl", "rb") as file:
-        PEOPLE_REPAIR = load(file)["PR"]
+        Brain = load(file)["KNOWLEDGE"]
 else:
-    PEOPLE_REPAIR = dict()
+    Brain = dict()
 
 
-def translate_num1(text: str, origin_lang: str = "en", output_lang: str = "zh-CN", tries: int = 1) -> str:
-    sleep(random()/1.5)
-    with catch_warnings():
-        simplefilter("ignore")
-        output = get("https://translate.appworlds.cn?text={}&from={}&to={}".format(text, origin_lang, output_lang),
-                     verify=False).json()
-    if output["code"] != 200 and tries < 3:
-        sleep(random())
-        return translate_num1(text, tries + 1)
-    elif output["code"] == 200:
-        try:
-            return output["data"]
-        except AttributeError:
-            sleep(random)
-            return translate_num1(text, tries + 1)
+def make_md5(s, encoding='utf-8'):
+    return md5(s.encode(encoding)).hexdigest()
+
+
+def translate(query: str, appid: str, appkey: str, from_lang: str = "auto", to_lang: str = "zh",
+              url: str = 'http://api.fanyi.baidu.com/api/trans/vip/translate',
+              salt: int = randint(32768, 65536), tries: int = 0) -> str:
+    global Brain, ERR_TRIES
+    r = post(url=url, headers={'Content-Type': 'application/x-www-form-urlencoded'},
+             params={'appid': appid, 'q': query, 'from': from_lang, 'to': to_lang,
+                     'salt': salt,
+                     'sign': make_md5(appid + query + str(salt) + appkey)}).json()
+    debug(r)
+    if "error_code" in r.keys() or "error_msg" in r.keys():
+        warning("错误代码：{}，错误信息：{}，出错文本：{}，尝试次数：{}".format(r["error_code"], r["error_msg"], query, tries))
+        if tries >= ERR_TRIES:
+            warning("次数超出限制，自动停止请求")
+            return query
+        else:
+            return translate(query=query, appid=appid, appkey=appkey, from_lang=from_lang, to_lang=to_lang, url=url,
+                             salt=randint(32768, 65536), tries=tries + 1)
     else:
+        result = r["trans_result"]
+    if len(result) == 1:
+        Brain[result[0]["src"]] = result[0]["dst"]
+    return result[0]["dst"]
+
+
+def smart_translate(text: str, appid: str, appkey: str) -> str:
+    if '\n' in text:
+        warning("无法适应带回车的字符：{}".format(text))
         return text
-
-
-def translate_num2(text: str, tries: int = 1) -> str:
-    sleep(random()/1.5)
-    try:
-        output = get("https://api.52vmy.cn/api/query/fanyi?msg={}".format(text))
-        if output.json()["code"] != 200 and tries < 3:
-            return translate_num1(text, tries=tries + 1)
-        elif output.json()["code"] == 200:
-            return output.json()["data"]['target']
-        else:
-            return text
-    except JSONDecodeError:
-        return translate_num3(text, tries=tries + 1)
-    except AttributeError:
-        return translate_num3(text, tries=tries + 1)
-
-
-def translate_num3(text: str, origin_lang: str = "en", output_lang: str = "zh", tries: int = 1) -> str:
-    sleep(random()/1.5)
-    with catch_warnings():
-        simplefilter("ignore")
-        output = get(
-            "https://translate.cloudflare.jaxing.cc/?text={}&source_lang={}&target_lang={}".format(text, origin_lang,
-                                                                                                   output_lang)).json()
-    try:
-        if output["code"] != 200 and tries < 3:
-            return translate_num1(text, tries=tries + 1)
-        elif output["code"] == 200:
-            try:
-                return output["data"]["response"]["translated_text"]
-            except AttributeError:
-                return translate_num1(text, tries=tries + 1)
-        else:
-            return text
-    except KeyError:
-        return translate_num1(text, tries=tries + 1)
-
-
-def smart_translate(text: str, repair_dict: dict = {}) -> str:
     global Brain
     out = str()
     try:
         split_text = text.split(' ')
     except AttributeError:
-        return translate_num3(text)
+        return translate(text, appid, appkey)
     else:
         for i in split_text:
-            if i in repair_dict.keys():
-                Brain[i] = repair_dict[i]
             if i in Brain.keys():
                 out += Brain[i]
             else:
-                give = translate_num1(i)
-                if i != give:
-                    out += give
-                else:
-                    give = translate_num2(i)
-                    if i != give:
-                        out += give
-                    else:
-                        give = translate_num3(i)
-                        if i != give:
-                            out += give
-                        else:
-                            return ""
-                Brain[i] = give
+                out += translate(i, appid, appkey)
         return out
 
-Brain = dict()
+
 DIR_MODE = input("选择启动模式\n0. 单文件处理模式\n1. 批量处理模式\n")
 FROM_FILE = SAVE_FILE = None
 if DIR_MODE == "0":
@@ -135,27 +97,31 @@ elif DIR_MODE == "2":
         print("Inside Trusted Test User")
         while True:
             if input("S mode or R mode") == "S":
-                FROM_FILE = r"D:\cty\python\uihans\testinput.ui"
-                SAVE_FILE = r"D:\cty\python\uihans\testoutput.ui"
+                FROM_FILE = r"testinput.ui"
+                SAVE_FILE = r"testoutput.ui"
                 tree = ET.parse(source=FROM_FILE)
                 root = tree.getroot()
                 with tqdm(root.iter('string'), desc=FROM_FILE, total=len(list(root.iter('string')))) as small_bar:
                     for elem in small_bar:
-                        # small_bar.write("开始翻译：{}".format(elem.text))
                         try:
-                            to = smart_translate(elem.text, PEOPLE_REPAIR)
-                            elem.text = to
-                        except KeyboardInterrupt:
-                            with open("settings.pkl", "wb") as file:
-                                dump({"PR": PEOPLE_REPAIR}, file)
-                            exit(-1)
-                        # small_bar.write("翻译完成：{}".format(to))
-                break
+                            # small_bar.write("开始翻译：{}".format(elem.text))
+                            try:
+                                to = smart_translate(text=elem.text, appid="20240714002099552",
+                                                     appkey="Ta2uj_UULC5VLMGYWXmZ")
+                                elem.text = to
+                            except KeyboardInterrupt:
+                                with open("settings.pkl", "wb") as file:
+                                    dump({"KNOWLEDGE": Brain}, file)
+                                exit(-1)
+                            # small_bar.write("翻译完成：{}".format(to))
+                        except BaseException as err:
+                            error(err)
+                    break
             else:
                 if input("R or W") == "W":
-                    PEOPLE_REPAIR[input("Key:")] = input("type")
+                    Brain[input("Key:")] = input("type")
                 else:
-                    print(PEOPLE_REPAIR)
+                    print(Brain)
 
     else:
         print("Something Wrong,Please Try again later")
@@ -178,29 +144,52 @@ print(r"""
 =====================================================================
 ---------------------------------------------------------------------""")
 
-
 if DIR_MODE == "1":
     FILE_LIST = list()
     for FILE_NAME in listdir(FROM_FILE):
         if isfile(FILE_NAME) and splitext(FILE_NAME)[1] in ['.ui', '.xml']:
             FILE_LIST.append(FILE_NAME)
     with tqdm(FILE_LIST) as big_bar:
-        for FILE_NAME in big_bar:
-            tree = ET.parse(source=FILE_NAME)
+        for file in big_bar:
+            tree = ET.parse(source=FROM_FILE+"\\"+file)
             root = tree.getroot()
-            with tqdm(root.iter('string'), desc=FROM_FILE, total=len(list(root.iter()))) as small_bar:
+            with tqdm(root.iter('string'), desc=FROM_FILE, total=len(list(root.iter('string')))) as small_bar:
                 for elem in small_bar:
-                    # small_bar.write("开始翻译：{}".format(elem.text))
-                    to = smart_translate(elem.text)
-                    elem.text = to
-                    # small_bar.write("翻译完成：{}".format(to))
-else:
+                    try:
+                        # small_bar.write("开始翻译：{}".format(elem.text))
+                        try:
+                            to = smart_translate(text=elem.text, appid="20240714002099552",
+                                                 appkey="Ta2uj_UULC5VLMGYWXmZ")
+                            elem.text = to
+                        except KeyboardInterrupt:
+                            with open("settings.pkl", "wb") as file:
+                                dump({"KNOWLEDGE": Brain}, file)
+                            exit(-1)
+                        # small_bar.write("翻译完成：{}".format(to))
+                    except BaseException as err:
+                        error(err)
+            tree.write(SAVE_FILE+"\\"+file, encoding="utf-8")
+    with open("settings.pkl", "wb") as file:
+        dump({"KNOWLEDGE": Brain}, file)
+    exit(0)
+elif DIR_MODE == "0":
     tree = ET.parse(source=FROM_FILE)
     root = tree.getroot()
     with tqdm(root.iter('string'), desc=FROM_FILE, total=len(list(root.iter('string')))) as small_bar:
         for elem in small_bar:
-            # small_bar.write("开始翻译：{}".format(elem.text))
-            to = smart_translate(elem.text)
-            elem.text = to
-            # small_bar.write("翻译完成：{}".format(to))
-tree.write(SAVE_FILE)
+            try:
+                # small_bar.write("开始翻译：{}".format(elem.text))
+                try:
+                    to = smart_translate(text=elem.text, appid="20240714002099552",
+                                         appkey="Ta2uj_UULC5VLMGYWXmZ")
+                    elem.text = to
+                except KeyboardInterrupt:
+                    with open("settings.pkl", "wb") as file:
+                        dump({"KNOWLEDGE": Brain}, file)
+                    exit(-1)
+                # small_bar.write("翻译完成：{}".format(to))
+            except BaseException as err:
+                error(err)
+tree.write(SAVE_FILE, encoding="utf-8")
+with open("settings.pkl", "wb") as file:
+    dump({"KNOWLEDGE": Brain}, file)
